7c75cd36695b5449c9a2d21d67ad72c4831e93a1
Aleksander Kolata <olek.tbg.konta@gmail.com>
2023-02-20 23:04:00 +0100

    frontend apps

---
+++ apps/frontend/second-frontend-app/src/app/app.component.ts
@@ -0,0 +1,88 @@
+import {Component, OnInit} from '@angular/core';
+import {AuthConfig, OAuthService} from "angular-oauth2-oidc";
+import {tap} from "rxjs";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import jwtDecode, { JwtPayload } from "jwt-decode";
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit {
+  authConfig: AuthConfig = {
+    issuer: 'http://localhost:9990/auth/realms/altimeet',
+    redirectUri: window.location.origin,
+    clientId: 'altimeet-frontend-second-app',
+    responseType: 'code',
+    scope: 'openid profile email',
+    showDebugInformation: true,
+    sessionChecksEnabled: true,
+  };
+
+  decodedAccessToken: any;
+  userInfo: any;
+  firstServiceResponse: any;
+  secondServiceResponsePassToken: any;
+  secondServiceResponseAsServiceClient: any;
+
+  constructor(public readonly oauthService: OAuthService, private readonly httpClient: HttpClient) {
+  }
+
+  ngOnInit() {
+    this.oauthService.configure(this.authConfig);
+    this.oauthService.loadDiscoveryDocumentAndTryLogin().then(
+      _ => {
+        if (this.oauthService.hasValidAccessToken()) {
+          this.oauthService.loadUserProfile()
+            .then(userInfo => this.userInfo = userInfo)
+            .catch(_ => this.userInfo = null);
+
+          this.decodedAccessToken = jwtDecode<JwtPayload>(this.oauthService.getAccessToken());
+        }
+      }
+    );
+
+    this.oauthService.events
+      .pipe(tap(event => {
+        console.log(event);
+      }))
+
+  }
+
+  logout() {
+    this.oauthService.revokeTokenAndLogout();
+  }
+
+  login() {
+    this.oauthService.initCodeFlow();
+  }
+
+  callFirstService() {
+    const headers: HttpHeaders = new HttpHeaders().set('Authorization', this.oauthService.authorizationHeader())
+    this.httpClient.get<any>('/api/first-service/private-resource', {headers})
+      .subscribe(
+        res => this.firstServiceResponse = res,
+        error => this.firstServiceResponse = error
+      )
+  }
+
+  callSecondServicePassToken() {
+    const headers: HttpHeaders = new HttpHeaders().set('Authorization', this.oauthService.authorizationHeader())
+    this.httpClient.get<any>('/api/second-service/call-first-service-pass-token', {headers})
+      .subscribe(
+        res => this.secondServiceResponsePassToken = res,
+        error => this.secondServiceResponsePassToken = error
+      )
+  }
+
+  callSecondServiceAsServiceClient() {
+    const headers: HttpHeaders = new HttpHeaders().set('Authorization', this.oauthService.authorizationHeader())
+    this.httpClient.get<any>('/api/second-service/call-first-service-as-service-client', {headers})
+      .subscribe(
+        res => this.secondServiceResponseAsServiceClient = res,
+        error => this.secondServiceResponseAsServiceClient = error
+      )
+  }
+
+}
---
+++ apps/frontend/second-frontend-app/src/app/app.module.ts
@@ -0,0 +1,31 @@
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {OAuthModule, OAuthStorage} from "angular-oauth2-oidc";
+import {HttpClientModule} from "@angular/common/http";
+import {PrettyPrintPipe} from "./prettyprint.pipe";
+
+export function storageFactory(): OAuthStorage {
+  return localStorage;
+}
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PrettyPrintPipe
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    OAuthModule.forRoot()
+  ],
+  providers: [
+    {provide: OAuthStorage, useFactory: storageFactory}
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+}
